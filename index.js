0813e3a1586cb384bd1f40925287eb7955e01d62
Kalib Riter <klaibriter@gmail.com>
2024-07-26 17:42:59 -0400

    passed tests

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function writeCards(names, holiday) {
+    const messages =[]
+     for (let i = 0; i < names.length; i++) {
+        console.log(`Thank you, ${names[i]}, for the wonderful ${holiday} gift!`)
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${holiday} gift!`)
+     }
+     return messages 
+}
+writeCards(["Kalib", "Abby", "Jordan"], "easter")
+
+function countDown(startingNumber) {
+    while (startingNumber >= 0) {
+        console.log(startingNumber--)
+    }
+}
+
+
+countDown(12)
+
+
